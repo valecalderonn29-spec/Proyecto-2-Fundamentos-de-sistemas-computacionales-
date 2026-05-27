793df0a126d53ee6f1483f4e7fbd7efe512f2c03
valecalderonn29-spec <valecalderonn29@gmail.com>
2026-05-27 16:57:53 -0600

    Create pruebas 7 segmentos.py

---
+++ pruebas 7 segmentos.py
@@ -0,0 +1,26 @@
+from machine import Pin
+
+pin_a = Pin(0,Pin.OUT)
+pin_b = Pin(1, Pin.OUT)
+pin_f = Pin(2,Pin.OUT)
+pin_g = Pin(3, Pin.OUT)
+pin_c = Pin(4,Pin.OUT)
+pin_d = Pin(5, Pin.OUT)
+pin_e = Pin(6, Pin.OUT)
+
+
+def imprimir uno {
+    pin_a.value(0)
+    pin_b.value(1)
+    pin_f.value(0)
+    pin_g.value(0)
+    pin_c.value(1)
+    pin_d.value(0)
+    pin_e.value(0)
+    
+    
+    }
+
+
+
+
